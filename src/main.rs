475bdb5448eeab476634d18d50a066abf2eadbbb
notrodes <reallystupidid@protonmail.com>
2021-02-15 14:33:16 -0500

    Set up serving static files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// #![feature(proc_macro_hygiene, decl_macro)]
+use rocket;
+use rocket_contrib::serve::StaticFiles;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", StaticFiles::from("public"))
+        .launch();
 }
