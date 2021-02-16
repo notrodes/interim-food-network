98e9bb80dadd5ababc06b15d3dd711cc915608dc
notrodes <reallystupidid@protonmail.com>
2021-02-15 23:17:35 -0500

    Begin to set up query responses from client for zip code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-// #![feature(proc_macro_hygiene, decl_macro)]
-use rocket;
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
 use rocket_contrib::serve::StaticFiles;
+// use rocket_contrib::databases::{r2d2, DbError, DatabaseConfig, Poolable};
+use sonic_channel::*;
+
+// impl Poolable for sonic_channel::ControlChannel {}
+
+#[get("/search")]
+fn search() -> &'static str {
+    ""
+}
 
 fn main() {
     rocket::ignite()
         .mount("/", StaticFiles::from("public"))
+        .mount("/", routes![search])
         .launch();
 }
