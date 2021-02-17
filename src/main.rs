9421953a50caf4b5ac3dbec55a4479a41f2bc7fa
notrodes <reallystupidid@protonmail.com>
2021-02-17 14:55:14 -0500

    Added query submitting functionality

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,34 @@
 #[macro_use]
 extern crate rocket;
 use rocket_contrib::serve::StaticFiles;
+use rocket::request::Form;
 // use rocket_contrib::databases::{r2d2, DbError, DatabaseConfig, Poolable};
 use sonic_channel::*;
 
 // impl Poolable for sonic_channel::ControlChannel {}
 
-#[get("/search")]
-fn search() -> &'static str {
-    ""
+#[derive(FromForm)]
+struct SearchQuery{
+    zip: usize
 }
+
+#[get("/search?<zip>")]
+fn search(zip: usize) {
+    println!("{}", zip);
+    // ""
+}
+
+
+
+// #[derive(FromForm)]
+// struct Task {
+//     complete: bool,
+//     description: String,
+// }
+
+// #[post("/todo", data = "<task>")]
+// fn new(task: Form<Task>) { /* .. */ }
+
 
 fn main() {
     rocket::ignite()
