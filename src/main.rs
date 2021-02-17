f6b77d6fa6491d0a13e89b14d551d1091c448b39
notrodes <reallystupidid@protonmail.com>
2021-02-17 18:27:55 -0500

    Added access to sonic on get request

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
-use rocket_contrib::serve::StaticFiles;
 use rocket::request::Form;
+use rocket::response::NamedFile;
+use rocket_contrib::serve::StaticFiles;
+use std::error::Error;
+// use std::io::Result;
 // use rocket_contrib::databases::{r2d2, DbError, DatabaseConfig, Poolable};
 use sonic_channel::*;
 
 // impl Poolable for sonic_channel::ControlChannel {}
 
-#[derive(FromForm)]
-struct SearchQuery{
-    zip: usize
-}
-
+// TODO: pool with r2d2
 #[get("/search?<zip>")]
-fn search(zip: usize) {
+fn search(zip: u64) -> Result<NamedFile, Box<dyn Error>> {
+    let channel = SearchChannel::start("localhost:1491", "password")?;
+    println!("{}", channel.ping()?);
+    channel.quit()?;
     println!("{}", zip);
-    // ""
+    Ok(NamedFile::open("public/results.html")?)
 }
-
-
 
 // #[derive(FromForm)]
 // struct Task {
@@ -29,7 +29,6 @@ fn search(zip: usize) {
 
 // #[post("/todo", data = "<task>")]
 // fn new(task: Form<Task>) { /* .. */ }
-
 
 fn main() {
     rocket::ignite()
