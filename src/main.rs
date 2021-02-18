61b5dd1c1c08dbffbff9a1777b5f7e7bbe528591
notrodes <reallystupidid@protonmail.com>
2021-02-18 18:38:27 -0500

    Added files for sqlite database

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
-use rocket::{ request::Form, response::NamedFile, http::Status };
+#[macro_use]
+extern crate rocket_contrib;
+use rocket::{http::Status, request::Form, response::NamedFile};
+use rocket_contrib::databases::diesel;
 use rocket_contrib::serve::StaticFiles;
 use std::error::Error;
 
 // use rocket_contrib::databases::{r2d2, DbError, DatabaseConfig, Poolable};
 use sonic_channel::*;
 
+#[get("/teapot")]
+fn teapot() -> Status {
+    Status::ImATeapot
+}
+
 // TODO: pool with r2d2
 #[get("/search?<zip>")]
 fn search(zip: u64) -> Result<NamedFile, Box<dyn Error>> {
     let channel = SearchChannel::start("localhost:1491", "password")?;
-    println!("{}", channel.ping()?);
+
     channel.quit()?;
     println!("{}", zip);
     Ok(NamedFile::open("public/results.html")?)
@@ -20,9 +28,8 @@ fn search(zip: u64) -> Result<NamedFile, Box<dyn Error>> {
 
 #[derive(FromForm, Debug)]
 struct Listing {
-    test: String
+    test: String,
 }
-
 
 #[post("/create.html", data = "<listing>")]
 fn create(listing: Form<Listing>) -> Status {
@@ -30,11 +37,13 @@ fn create(listing: Form<Listing>) -> Status {
     Status::Ok
 }
 
-
+#[database("my_db")]
+struct MyDatabase(diesel::SqliteConnection);
 
 fn main() {
     rocket::ignite()
         .mount("/", StaticFiles::from("public"))
-        .mount("/", routes![search, create])
+        .mount("/", routes![search, create, teapot])
+        .attach(MyDatabase::fairing())
         .launch();
 }
