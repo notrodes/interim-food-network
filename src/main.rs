89c28e2ac3314a14410d28c71178dd3d6be3dd7e
notrodes <reallystupidid@protonmail.com>
2021-02-19 13:45:47 -0500

    Got handlebar rendering working

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
 
-use rocket::{http::Status, request::Form, response::NamedFile};
+use rocket::{http::Status, request::Form};
 use rocket_contrib::databases::rusqlite;
-use rocket_contrib::templates::Template;
-// use rocket_contrib::databases::rusqlite:;
 use rocket_contrib::serve::StaticFiles;
-// use diesel::prelude::*;
+use rocket_contrib::templates::handlebars;
+use rocket_contrib::templates::Template;
+use serde::Serialize;
 use std::error::Error;
 
-use sonic_channel::*;
+// use sonic_channel::*;
 
 #[get("/teapot")]
 fn teapot() -> Status {
@@ -26,33 +26,42 @@ fn search(conn: MyDatabase, zip: u32) -> Result<Template, Box<dyn Error>> {
 
     // channel.quit()?;
     let mut query = conn.prepare("SELECT * from listings where zip=?")?;
-    let results = query.query_map(&[&zip], |row| row.get(1)?)?;
-
-    // println!("{}", results);
-    Ok(Template::render("public/results.hbs", results))
+    let results: Vec<Listing> = query
+        .query_map(&[&zip], |row| row.get(2))?
+        .map(|name| Listing {
+            zip,
+            name: name.unwrap(),
+        })
+        .collect();
+    // let mut listings = Vec::new();
+    // for row in results {
+    //     listings.push(Listing {zip, name: row?});
+    // }
+    let serialized = json!({ "listings": &results, "zip": zip });
+    Ok(Template::render("results", serialized))
 }
 
-#[derive(FromForm, Debug)]
-struct FormListing {
-    zip: String,
+#[derive(Serialize, FromForm, Debug)]
+struct Listing {
+    zip: u32,
     name: String,
 }
 
 #[post("/create.html", data = "<listing>")]
-fn create(conn: MyDatabase, listing: Form<FormListing>) -> rusqlite::Result<Status> {
+fn create(conn: MyDatabase, listing: Form<Listing>) -> rusqlite::Result<Status> {
     // conn.get_one();
     // If there is a table with the zip code
     // panic!();
     // println!("{:?}", conn
-    // .prepare("SELECT * FROM zip_codes WHERE code = (?1)")? 
+    // .prepare("SELECT * FROM zip_codes WHERE code = (?1)")?
     // .exists(&[&listing.zip]) );
     println!("{:?}", listing);
-    
-        // panic!("1");
-        conn.execute(
-            "INSERT INTO listings (zip, name) VALUES (?1, ?2)",
-            &[&listing.zip, &listing.name],
-        )?;
+
+    // panic!("1");
+    conn.execute(
+        "INSERT INTO listings (zip, name) VALUES (?1, ?2)",
+        &[&listing.zip, &listing.name],
+    )?;
     // println!("{:?}", );
     Ok(Status::Ok)
 }
